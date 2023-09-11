befc77bffe47dfc864359579c5cd4fc7820fb026
egemonyeemelie@gmail.com <egemonyeemelie@gmail.com>
2023-09-11 13:14:30 +0100

    update

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ let today = date.getDay();
 let daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let dayDisplay = document.querySelector('#week');
 dayDisplay.innerHTML = daysOfTheWeek[today];
-//rendering the UTC time to the DOM
-let showTime=()=>{
-  let date = new Date();
-  let milliseconds = Date.now();
-  let timeDisplay = document.querySelector('#time');
-  let utctimeDisplay;
-  
-  milliseconds = milliseconds<10?`0${milliseconds}`:milliseconds;
-  utctimeDisplay = milliseconds;
 
-  timeDisplay.innerHTML=utctimeDisplay;
-  
-  
-  setTimeout(showTime,100);
+//rendering the UTC time to the DOM
+let showTime = () => {
+   let date = new Date();
+   let milliseconds = Date.now();
+   let timeDisplay = document.querySelector('#time');
+   let utctimeDisplay;
+
+   milliseconds = milliseconds < 10 ? `0${milliseconds}` : milliseconds;
+   utctimeDisplay = milliseconds;
+
+   timeDisplay.innerHTML = utctimeDisplay;
+
+
+   setTimeout(showTime, 100);
 };
 
 showTime();
